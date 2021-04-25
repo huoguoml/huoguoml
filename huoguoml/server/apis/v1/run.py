4c3ac0e0b6f1fa707d3c08c0e27be04cb309ec13
steven-mi <stevenmi@live.de>
2021-04-25 19:14:02 +0200

    refactor: create class for every external router

--- huoguoml/server/apis/v1/run.py
+++ huoguoml/server/apis/v1/run.py
@@ -5,23 +5,24 @@ from huoguoml.schemas.run import RunIn, Run
 from huoguoml.server.db.service import Service
 
 
-def get_router(service: Service) -> APIRouter:
-    router = APIRouter(
-        prefix="/api/v1/runs",
-        tags=["runs"],
-    )
+class RunRouter(object):
+    def __init__(self, service: Service):
+        router = APIRouter(
+            prefix="/api/v1/runs",
+            tags=["runs"],
+        )
 
-    @router.get("/{run_id}")
-    async def get_run(run_id: str):
-        run_file_path = service.get_run_file_path(run_id=run_id)
-        return FileResponse(run_file_path, media_type='application/zip')
+        @router.get("/{run_id}")
+        async def get_run(run_id: str):
+            run_file_path = service.get_run_file_path(run_id=run_id)
+            return FileResponse(run_file_path, media_type='application/zip')
 
-    @router.post("")
-    async def create_run(run: RunIn):
-        return service.create_run(run=run)
+        @router.post("")
+        async def create_run(run: RunIn):
+            return service.create_run(run=run)
 
-    @router.put("")
-    async def update_run(run: Run):
-        return service.update_run(run=run)
+        @router.put("")
+        async def update_run(run: Run):
+            return service.update_run(run=run)
 
-    return router
+        self.router = router
