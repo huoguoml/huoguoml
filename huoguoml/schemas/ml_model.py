e2e04de300b74390a688b329cfd91f12ef4b645e
steven-mi <stevenmi@live.de>
2021-04-25 23:46:42 +0200

    feat: register model button

---
+++ huoguoml/schemas/ml_model.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, constr
+
+from huoguoml.schemas.run import Run
+
+
+class MLModelIn(BaseModel):
+    name: constr(to_lower=True)
+    run: Optional[Run]
+
+
+class MLModel(BaseModel):
+    name: constr(to_lower=True)
+    runs: List[Run]
+
+    class Config:
+        orm_mode = True
